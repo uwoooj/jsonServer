0378feb981fb59a3b4652fa1aea6322cad1a4070
김유진 <yujin0154@gmail.com>
2023-09-05 15:03:51 +0900

    sdfsdf

--- index.js
+++ index.js
@@ -9,22 +9,35 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const data = {
+  select: function () {
+    return JSON.parse(fs.readFileSync("./test.json"));
+  },
+  insert: function (newObj) {
+    const jsonData = data.select();
+    const newData = [...jsonData, { id: jsonData.length + 1, ...newObj }];
+    fs.writeFileSync("./test.json", JSON.stringify(newData));
+    return newData;
+  },
+  update: function () {},
+  delete: function () {},
+};
+
 app.get("/abc", function (req, res) {
-  const jsonData = fs.readFileSync("./test.json");
-  res.send(JSON.parse(jsonData));
+  res.send(data.select());
 });
-app.get("/abc/:id", function (req, res) {
-  const jsonData = fs.readFileSync("./test.json");
-  const data = JSON.parse(jsonData);
+
+app.delete("/abc/:id", function (req, res) {
+  const jsonData = data.select();
+
   const { id } = req.params;
-  const aaa = data.filter((n) => n.id == id);
-  res.send(aaa);
+  const newData = jsonData.filter((n) => n.id != id);
+  fs.writeFileSync("./test.json", JSON.stringify(newData));
+  res.send(newData);
 });
 
 app.post("/insert", function (req, res) {
-  fs.writeFileSync("./test.json", JSON.stringify(req.body));
-  console.log(req.body);
-  res.send("성공");
+  res.send(data.insert(req.body));
 });
 
-app.listen(3030);
+app.listen(3000);
